80033060a7ff99c2f369e296d0e31f97d08f9fd3
Rana Rashid <rrashid.physics@gmail.com>
2024-09-07 18:19:43 +0500

    Assignment-3 JS
    Done

---
+++ app.js
@@ -0,0 +1,171 @@
+// Assignment-3
+
+// Q1
+
+// var city= prompt("Please Enter Name of City");
+// if (city==="Karachi") { document.write("Welcome to the city of Lights");
+    
+// } else{
+//     document.write("Welcome to "+ city);
+// }
+
+// Q2
+
+
+// var gender= prompt("Please Enter Your Gender");
+// if (gender==="Male") { document.write("Good Morning Sir");
+    
+// } else if (gender==="Female") { document.write("Good Morning Madam")}
+// else
+// {
+//     document.write("Please Enter Valid Gender");
+// }
+
+// Q3
+
+
+// var signalColor= prompt("Please Enter Signal color");
+// if (signalColor==="Red") { document.write("Must Stop");
+    
+// } else if (signalColor==="Yellow") { document.write("Ready to Move")}
+// else if (signalColor==="Green") { document.write("Move Now")}
+// else
+// {
+//     document.write("Please Enter Signal Color, Red, Yellow or Green");
+// }
+
+//Q4
+
+// var fuel= prompt("Please Enter remaining fuel in Car in liters");
+// if (fuel<"0.25liters") { document.write("Please refill the fuel in your car");
+    
+// } 
+// else
+// {
+//     document.write("Enjoy Driving");
+// }
+
+//Q5
+
+//a. 
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+
+//b.
+
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+//c.
+//  var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+//d. 
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+//e.
+//  if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+
+
+//f.
+//  if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+
+//Q6
+
+// var maths = prompt("enter maths marks");
+// var comp = prompt("enter computer marks");
+// var chem = prompt("enter chemistry marks");
+// var obt_marks = maths + comp + chem;
+// var totalMarks = 300;
+// var per = (obt_marks / totalMarks) * 100;
+
+// if (per >= 80){
+//     alert("Grade: A-one \n Remarks: Excellent");
+// }
+// else if (per >= 70){
+//     alert("Grade: A \n Remarks: Good");
+// }
+// else if (per >= 60){
+//     alert("Grade: B \n Remarks: You need to improve");
+// }
+// else if (per < 60){
+//     alert("Grade: Fail \n Remarks: Sorry")
+// }
+
+
+// question7
+// var secretNumber = 7;
+// var guessNumber = prompt("enter your number");
+// if (guessNumber == secretNumber){
+//     alert("Bingo! Correct answer");
+// }
+// else if (secretNumber == ++guessNumber){
+//     alert("Close enough to the correct answer");
+// }
+
+// question8
+// var no = prompt("enter your number");
+// if (no % 3 == 0){
+//     alert("The number is Completely divisible by 3");
+// }
+// else{
+//     alert("not divisible");
+// }
+
+// Q 9
+// var no = prompt("enter your number");
+// if (no % 2 == 0){
+//     alert("The number is even");
+// }
+// else{
+//     alert("The number is odd");
+// }
+
+// Q 11
+// var firstNumber = +prompt("enter first number");
+// var sign = prompt("enter operator");
+// var secondNumber = +prompt("enter second number");
+
+
+
+// if (sign == "+"){
+//     alert(firstNumber + secondNumber);
+// }
+// else if (sign == "-"){
+//     alert(firstNumber - secondNumber);
+// }
+// else if (sign == "*"){
+//     alert(firstNumber * secondNumber);
+// }
+// else if (sign == "/"){
+//     alert(firstNumber / secondNumber);
+// }
+// else if (sign == "%"){
+//     alert(firstNumber % secondNumber);
+// }
